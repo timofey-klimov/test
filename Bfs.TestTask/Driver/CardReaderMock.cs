400d07039ccc7a6597c76966498153c809ecaed2
tklimov <timofey.klimov@inbox.ru>
2024-08-13 23:15:14 +0300

    тестовое

--- Bfs.TestTask/Driver/CardReaderMock.cs
+++ Bfs.TestTask/Driver/CardReaderMock.cs
@@ -1,31 +1,73 @@
 using System.Runtime.CompilerServices;
+using System.Threading.Channels;
 
 namespace Bfs.TestTask.Driver;
 
 public class CardDriverMock : ICardDriverMock
 {
-    public async Task<CardData?> ReadCard(CancellationToken cancellationToken)
+    private readonly Channel<CardDriverContext> _contextChannel;
+
+    public CardDriverMock()
     {
-      throw new NotImplementedException();
+        _contextChannel = Channel.CreateUnbounded<CardDriverContext>();
     }
 
-    public IAsyncEnumerable<EjectResult> EjectCard(CancellationToken cancellationToken)
+    public async Task<CardData?> ReadCard(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var ctx = await _contextChannel.Reader.ReadAsync(cancellationToken);
+            return ctx.Card;
+
+        } catch (OperationCanceledException)
+        {
+            return default;
+        }
+    }
+
+    public async IAsyncEnumerable<EjectResult> EjectCard(CancellationToken cancellationToken)
+    {
+        await _contextChannel.Writer.WriteAsync(CardDriverContext.Ejected());
+        do
+        {
+            var item = await _contextChannel.Reader.ReadAsync();
+            yield return item.Result;
+        } while (!cancellationToken.IsCancellationRequested);
+        
+        _contextChannel.Writer.Complete();
+        yield return EjectResult.Retracted;
     }
 
     public void SetCardData(CardData cardData)
     {
-        throw new NotImplementedException();
+        _contextChannel.Writer.WriteAsync(CardDriverContext.ReadCard(cardData));
     }
 
     public void CantReadCard()
     {
-        throw new NotImplementedException();
+        _contextChannel.Writer.WriteAsync(CardDriverContext.CantReadCard());
     }
 
     public void TakeCard()
     {
-        throw new NotImplementedException();
+        _contextChannel.Writer.WriteAsync(CardDriverContext.CardTaken());
+    }
+
+    private class CardDriverContext
+    {
+        public CardData? Card { get; private set; }
+
+        public EjectResult Result { get; private set; }
+
+        private CardDriverContext(CardData? card, EjectResult result)
+        {
+            Card = card;
+            Result = result;
+        }
+
+        public static CardDriverContext Ejected() => new CardDriverContext(default, EjectResult.Ejected);
+        public static CardDriverContext ReadCard(CardData cardData) => new CardDriverContext(cardData, EjectResult.Ejected);
+        public static CardDriverContext CantReadCard() => new CardDriverContext(default, EjectResult.Ejected);
+        public static CardDriverContext CardTaken() => new CardDriverContext(default, EjectResult.CardTaken);
     }
 }
--- Bfs.TestTask/Parser/Parser.cs
+++ Bfs.TestTask/Parser/Parser.cs
@@ -1,46 +1,133 @@
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading.Channels;
 
 namespace Bfs.TestTask.Parser;
 
 public class Parser : IParser
 {
-    public IAsyncEnumerable<IMessage> Parse(ChannelReader<ReadOnlyMemory<byte>> source)
+    private const byte Separator = 28;
+    private int _length;
+    private int messageCount = 0;
+    public async IAsyncEnumerable<IMessage> Parse(ChannelReader<ReadOnlyMemory<byte>> source)
     {
-        //Перед каждым сообщением первые 2 байта определяют его длину
-        //array[0] = (byte)(message.Length / 256);
-        //array[1] = (byte)(message.Length % 256);
+        while (source.TryRead(out var memoryBuffer))
+        {
+            int? messageType = null;
+            var messageLengthBuffer = memoryBuffer.Slice(0, 2).ToArray();
+            _length = messageLengthBuffer[0] * 256 + messageLengthBuffer[1];
+            var bodyBuffer = new byte[_length];
+            var messageLength = _length;
+            if (memoryBuffer.Length >= 3)
+            {
+                messageType = GetMessageType(memoryBuffer);
+                ParsePartBody(bodyBuffer, memoryBuffer.Slice(2), ref messageLength);
+            }
 
-        throw new NotImplementedException();
+            while (messageLength > 0)
+            {
+                memoryBuffer = await source.ReadAsync();
+                if (messageType is null)
+                {
+                    messageType = GetMessageType(memoryBuffer);
+                }
+                ParsePartBody(bodyBuffer, memoryBuffer, ref messageLength);
+            }
+
+            yield return messageType switch
+            {
+                0 => ParseCardReaderState(bodyBuffer.AsMemory().Slice(3)),
+                1 => ParseOtherMessages(bodyBuffer.AsMemory().Slice(3)),
+                _ => throw new InvalidOperationException()
+            };
+
+            await source.WaitToReadAsync();
+        }
+    }
+
+    private void ParsePartBody(byte[] dest, ReadOnlyMemory<byte> source, ref int length)
+    {
+        source.CopyTo(dest.AsMemory(_length - length));
+        length -= source.Length;
+    }
+
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static int? GetMessageType(ReadOnlyMemory<byte> buffer)
+    {
+        int? messageType;
+        var firstBitBuffer = buffer.Slice(2, 1);
+        var firstBit = Encoding.ASCII.GetString(firstBitBuffer.Span);
+        if (firstBit == "1")
+            messageType = 0;
+        else
+            messageType = 1;
+        return messageType;
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private IMessage ParseCardReaderState(ReadOnlyMemory<byte> buffer)
+    {
+        //00100355D1001
+        GetLuno(buffer, out var luno, out var separatorIndex);
+        var state = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 2).Span);
+        return new CardReaderState(luno, state[0], state[1].ToInt(), state[2].ToInt(), state[3].ToInt(), state[4].ToInt());
+
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static void GetLuno(ReadOnlyMemory<byte> buffer, out string luno, out int separatorIndex)
+    {
+        separatorIndex = buffer.Span.IndexOf(Separator);
+        luno = Encoding.ASCII.GetString(buffer.Slice(0, separatorIndex).Span);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private IMessage ParseOtherMessages(ReadOnlyMemory<byte> buffer)
+    {
+        var separatorCount = buffer.Span.Count(Separator);
+        if (separatorCount == 4)
+        {
+            return ParseSendStatus(buffer);
+        }
+
+        return ParseGetFitnessData(buffer);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private IMessage ParseSendStatus(ReadOnlyMemory<byte> buffer)
+    {
+        GetLuno(buffer, out var luno, out var separatorIndex);
+        var statusDescriptor = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 2, 1).Span)[0];
+        var tranNumber = int.Parse(Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 5).Span));
+        return new SendStatus(luno, statusDescriptor, tranNumber);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private IMessage ParseGetFitnessData(ReadOnlyMemory<byte> buffer)
+    {
+        GetLuno(buffer, out var luno, out var separatorIndex);
+        var statusDescriptor = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 2, 1).Span)[0];
+        var messageIdentifier = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 4, 1).Span)[0];
+        var hardwareIdentifier = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 5, 1).Span)[0];
+        var statusInformation = Encoding.ASCII.GetString(buffer.Slice(separatorIndex + 6).Span);
+        var fitness = statusInformation.Split('\u001d').Select(x => new FitnessState(x[0], x[1..]));
+
+        return new GetFitnessData(luno, statusDescriptor,messageIdentifier, hardwareIdentifier, fitness.ToArray());
+    }
+}
+
+public static class Extensions
+{
+    public static int ToInt(this char c)
+    {
+        return (int)(c - '0');
     }
 }
 
  /*
-       Message with type Card Reader State builded:
-       1200100355D1001
-       Description:
-       (b) 1 = Message class
-       (c) 2 = Message sub-class
-       (d) 00100355 = LUNO
-       CardReaderStateDto { Solicited = False, DeviceIdCode = D, SupplyState = NoOverfillCondition, Status = TimeOutCardHolderTakingCard, Severity = NoError }
-       (g1) D = Device Identifier Graphic
-       (g2) 1 = Device Status (TimeOutCardHolderTakingCard)
-       (g3) 0 = Error Severity (NoError)
-       (g4) 0 = Diagnostic Status
-       (g5) 1 = Supplies Status (NoOverfillCondition)
-
-
-       Message with type Send Status builded:
-       2200100355B4321
-       Description:
-       (b) 2 = Message class
-       (c) 2 = Message sub-class
-       (d) 00100355 = LUNO
-       Status data
-       (f) B = Status Descriptor (TransactionReplyReady)
-       Status Information
-       (g1) 4321 = Transaction number
-
-
        Message with type Get Fitness Data builded:
        2200100355FJAD01y1A0E00000G0L0w00040003000200010H0
        Description:
